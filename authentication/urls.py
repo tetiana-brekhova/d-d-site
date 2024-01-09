bc410b12cd2b6cd68881f70b3a9068089c4f8655
Tetiana <100039018+tetiana-brekhova@users.noreply.github.com>
2024-01-09 20:37:48 +0200

    Delete authentication directory

--- authentication/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('registration/', views.register),
-    path('log_in', views.log_in),
-    path("logout/", views.log_out),
-]
